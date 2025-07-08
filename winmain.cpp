22f4d9b97ebc3d41b8bea7399385e7c8106c5f38
crist <crist@CSI_LAPTOP>
2025-07-07 20:59:30 -0600

    update name Add project files.

---
+++ winmain.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include "Settings.h"
+
+int main(){
+    Settings S;
+    S.LoadFromFile("./External/Data/settings_view.txt");
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        S.draw(window);
+        window.display();
+    }
+}
